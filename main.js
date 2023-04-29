fc2a8244ba46cc8703229cc4e29600138afad1f3
Arturnyak <arturnyak98@gmail.com>
2023-04-29 18:55:36 +0400

    contextBridge

--- main.js
+++ main.js
@@ -1,30 +1,51 @@
-const { app, BrowserWindow, ipcMain } = require( "electron" );
-const path                            = require( "path" );
+const { app, BrowserWindow, ipcMain, Menu } = require( "electron" );
+const path = require( "path" );
 
 const createWindow = () => {
-    const win = new BrowserWindow( {
-        width: 800,
-        height: 600,
-        webPreferences: {
-            preload: path.join( __dirname, "preload.js" ),
-        },
-    } );
-    ipcMain.handle( "ping", () => "pong" );
-    win.loadFile( "index.html" );
+	const win = new BrowserWindow( {
+		width: 800,
+		height: 600,
+		webPreferences: {
+			contextIsolation: true,
+			enableRemoteModule: false,
+			preload: path.join( __dirname, "preload.js" ),
+		},
+	} );
+	win.loadFile( "index.html" );
+	const menu = Menu.buildFromTemplate( [
+		{
+			label: "Developer",
+			submenu: [
+				{ role: "reload" },
+				{ role: "forcereload" },
+				{ role: "separator" },
+				{
+					label: "Toggle DevTools",
+					accelerator: process.platform === "darwin" ? "Alt+Command+I" : "Ctrl+Shift+C",
+					click: () => {
+						win.webContents.toggleDevTools();
+					},
+				},
+			],
+		},
+	] );
+
+	Menu.setApplicationMenu( menu );
 };
 
 app.whenReady().then( () => {
-    createWindow();
+	createWindow();
+	ipcMain.handle( "ping", () => "pong" );
 
-    app.on( "activate", () => {
-        if ( BrowserWindow.getAllWindows().length === 0 ) {
-            createWindow();
-        }
-    } );
+	app.on( "activate", () => {
+		if ( BrowserWindow.getAllWindows().length === 0 ) {
+			createWindow();
+		}
+	} );
 } );
 
 app.on( "window-all-closed", () => {
-    if ( process.platform !== "darwin" ) {
-        app.quit();
-    }
+	if ( process.platform !== "darwin" ) {
+		app.quit();
+	}
 } );
--- renderer.js
+++ renderer.js
@@ -1,9 +1,11 @@
-const information = document.getElementById('info')
-information.innerText = `This app is using Chrome (v${versions.chrome()}), Node.js (v${versions.node()}), and Electron (v${versions.electron()})`
+const information = document.getElementById( "info" );
+information.innerText = `This app is using Chrome (v${versions.chrome()}), Node.js (v${versions.node()}), and Electron (v${versions.electron()})`;
 
 const func = async () => {
-    const response = await window.versions.ping()
-    console.log(response) // prints out 'pong'
-}
+	const response = await window.versions.ping();
+	console.log( response ); // prints out 'pong'
+};
 
-func()
+console.log( window.versions.myAPI );
+
+func();
